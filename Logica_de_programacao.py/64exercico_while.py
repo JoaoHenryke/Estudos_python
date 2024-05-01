542eb41a90115c27608fb76ce6ff83d8ea2fbeeb
Henrique <joaoh9121@outlook.com>
2024-04-30 22:53:18 -0300

    30-04-24

---
+++ Logica_de_programacao.py/64exercico_while.py
@@ -0,0 +1,22 @@
+"""
+Iterando strings com while
+
+"""
+#.......01234567
+#nome = "Henrique" #iteraveis
+#.......76543210
+#tamanho_nome = len(nome)
+#print(nome)
+#print([tamanho_nome])
+#print(nome[3])
+
+nome = 'Henrique'
+indice = 0
+novo_nome = ''
+
+
+while indice < len(nome):
+    letra = novo_nome[indice]
+    novo_nome += 
+
+print("Acabou!")
